c9f4f16a82010a8d6d91dcc509f40757f7fb56ad
therman mcmillan <mcmillanj0106@yahoo.com>
2021-12-06 19:15:57 -0500

    24 working

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,57 @@
+const db = require('../../data/db-config')
+
+
+
 const getAll = () => {
   // DO YOUR MAGIC
+  const accounts = db('accounts')
+  return accounts
 }
 
-const getById = id => {
+ const getById = id => {
   // DO YOUR MAGIC
+  const account =  db('accounts')
+  .where('id', id)
+  .first()
+  return account
 }
 
-const create = account => {
+const create = async account => {
   // DO YOUR MAGIC
+  const [ id ] = await  db('accounts')
+  .insert({
+    name: account.name.trim(),
+    budget: account.budget
+  })
+ const newAccount = 
+ getById(id)
+ return newAccount
 }
 
-const updateById = (id, account) => {
+const updateById = async (id, account) => {
   // DO YOUR MAGIC
+ 
+  await db('accounts')
+        .where('id',id)
+        .update(account)
+       return getById(id)
+  
+
 }
 
-const deleteById = id => {
+
+const deleteById = async (id) => {
   // DO YOUR MAGIC
-}
+  
+      const deletedAccount = await getById(id)
+  
+      await db('accounts')
+        .where({ id })
+        .del()
+       return deletedAccount
+  }
+
+
 
 module.exports = {
   getAll,
--- api/server.js
+++ api/server.js
@@ -1,7 +1,26 @@
 const express = require("express");
-
+const accountsRouter = require("./accounts/accounts-router")
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountsRouter)
+
+
+
+const errorHandling = (err, req, res, next) => {
+    const status = err.status || 500
+    res.status(status).json({
+        message: err.message,
+})
+}
+
+
+const notFound = (req, res, next) => {
+    res.status(404).json({
+        message: 'Not found'
+})
+}
+server.use(errorHandling)
+server.use('*', notFound)
 
 module.exports = server;
